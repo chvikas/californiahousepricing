8adb79eda54c2afa5996134f810f2193980321d6
Vikas Chaudhary <chvikas.ai@gmail.com>
2024-03-13 23:13:50 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def predict_api():
 def predict():
     data=[float(x) for x in request.form.values()]
     final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
+    # print(final_input)
     output=regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
@@ -33,15 +33,3 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# from flask import Flask, render_template
-#
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
